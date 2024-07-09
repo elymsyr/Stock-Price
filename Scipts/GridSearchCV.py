4dcb4f5ff37de268102c895ed33a43459b8556be
Orhun Eren Yalçınkaya <orhun868@gmail.com>
2024-07-10 01:05:28 +0300

    LSTM_Tests Updated data.py Added Scaler will be checked

--- Scipts/GridSearchCV.py
+++ Scipts/GridSearchCV.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import GridSearchCV
 from keras import Sequential
 from keras.src.layers import LSTM, Dense, Input
 
-data_size = 100
+data_size = 1000
 df = pd.read_csv('Data\AAPL_stock_prices.csv', delimiter=',')
 df = df.iloc[-data_size:, :]
 dates = pd.to_datetime(df['Date'])
@@ -41,10 +41,20 @@ def create_dataset(data: np.ndarray, time_step: int=10):
 
 X, Y, feature_number, time_step = create_dataset(data=scaled_data)
 
+print(X.shape, Y.shape)
+
+# Split the data into training and testing sets
+train_size = int(len(X) * 0.7)
+test_size = len(X) - train_size
+X_train, X_test = X[0:train_size], X[train_size:len(X)]
+Y_train, Y_test = Y[0:train_size], Y[train_size:len(Y)]
+
+print(X_train.shape, Y_train.shape)
+print(X_test.shape, Y_test.shape)
 
 def create_lstm_model(neurons=64, loss='mean_squared_error', activation='linear', optimizer='Adam', batch_size=1, epochs=50):
     model = Sequential()
-    model.add(LSTM(units=neurons, return_sequences=True, activation=activation, input_shape=Input(shape=(10,6))))
+    model.add(LSTM(units=neurons, return_sequences=True, activation=activation, input_shape=(X.shape[1], feature_number)))
     model.add(LSTM(units=neurons, return_sequences=False))
     model.add(Dense(1))
     model.compile(loss=loss, optimizer=optimizer)
@@ -63,13 +73,29 @@ def create_lstm_model(neurons=64, loss='mean_squared_error', activation='linear'
 param_grid = {
     'model__optimizer': ['adam'], #  'RMSprop', 
     'model__loss': ['mean_squared_error'],
-    'batch_size': [1, 6],
-    'epochs': [50],
+    'batch_size': [6],
+    'epochs': [1],
     'model__neurons': [64, 128],
-    'model__activation': ['relu', 'tanh', 'sigmoid', 'linear']
+    'model__activation': ['linear'] # 'relu', 'tanh', 'sigmoid', 
 }
-lstm_regressor = KerasRegressor(model=create_lstm_model, batch_size=1, epochs=50, verbose=0)
+
+param_grid = {
+    'model__optimizer': ['adam'], #  'RMSprop', 
+    'model__loss': ['mean_squared_error'],
+    'batch_size': [6],
+    'epochs': [1],
+    'model__neurons': [64, 128],
+    'model__activation': ['linear'] # 'relu', 'tanh', 'sigmoid', 
+}
+
+
+
+lstm_regressor = KerasRegressor(model=create_lstm_model, batch_size=1, epochs=1, verbose=1)
 # Create the GridSearchCV object
-grid_search = GridSearchCV(estimator=lstm_regressor, param_grid=param_grid, scoring='accuracy', error_score='raise', cv=3)
+grid_search = GridSearchCV(estimator=lstm_regressor, param_grid=param_grid, scoring='neg_mean_absolute_error', error_score='raise', cv=3)
 # Fit the grid search to the data
-grid_search.fit(X, Y)
+grid_search.fit(X_train, Y_train[:, target_column_index].reshape(-1,1))
+
+print(grid_search.best_params_)
+
+print(grid_search.best_score_)
