678d6799d8de493e1dc20a8b0a882e73fd028630
Orhun Eren Yalçınkaya <orhun868@gmail.com>
2024-07-29 18:57:09 +0300

    Formation Updated

--- Project/Time Series Data/formation.py
+++ Project/Time Series Data/formation.py
@@ -9,8 +9,8 @@ interpolated_time_series = interpolate_time_series(time_series)
 
 # # Plotting the interpolated time series data
 # plt.figure(figsize=(10, 5))
-# plt.plot(interpolated_time_series['Timestamp'], interpolated_time_series['Close'], label='Interpolated Data', linestyle='-', color='gray')
-# plt.scatter(time_series['Timestamp'], time_series['Close'], color='black', label='Original Data Points', marker='o')
+# plt.plot(interpolated_time_series.index, interpolated_time_series['close'], label='Interpolated Data', linestyle='-', color='gray')
+# plt.scatter(time_series['Timestamp'], time_series['Value'], color='black', label='Original Data Points', marker='o')
 # plt.title('Interpolated Time Series with Original Data Points')
 # plt.xlabel('Timestamp')
 # plt.ylabel('Close')
@@ -22,28 +22,9 @@ time = list(range(1, len(interpolated_time_series) + 1))
 df = interpolated_time_series
 distances = [40,60,80,100,120,140,160,180,200]
 
-plt.figure(figsize=(30, 25))
+plt.figure(figsize=(15, 10))
 
 for i, distance in enumerate(distances):
-    # maxima, minima = find_local_extrema(data, time, distance)
-    
-    # merged_list = sorted(minima + maxima, key=lambda x: x[0])
-    
-    # # Extracting indices and values for plotting
-    # indices = [item[0] for item in merged_list]
-    # values = [item[1] for item in merged_list]
-    # plt.figure(figsize=(10, 5))
-    
-    # plt.plot(indices, values, marker='o')
-    # plt.plot(time, data, label='Data')
-    # plt.scatter(*zip(*maxima), color='red', label='Maxima')
-    # plt.scatter(*zip(*minima), color='blue', label='Minima')
-
-    # plt.title(f'Local Maxima and Minima with Distance {distance}')
-    # plt.xlabel('Time')
-    # plt.ylabel('Data Value')
-    # plt.legend()
-    
     plt.grid(True)
     plt.subplot(3, 3, i + 1)
     peaks_idx, troughs_idx = find_extramas(df, window_length=int(distance/2), width=int(distance/10), distance=15)
@@ -55,7 +36,7 @@ for i, distance in enumerate(distances):
     peaks, = plt.plot(df.index[peaks_idx], df.close_smooth.iloc[peaks_idx], c="r", linestyle='None', markersize = 10.0, marker = "o", zorder=10)
     troughs, = plt.plot(df.index[troughs_idx], df.close_smooth.iloc[troughs_idx], c="g", linestyle='None', markersize = 10.0, marker = "o", zorder=10)
 
-plt.tight_layout()
+plt.subplots_adjust(wspace=0.015, hspace=0.14)
 plt.show()
 
 
